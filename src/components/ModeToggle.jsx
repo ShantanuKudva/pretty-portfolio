fb2a4acbb5be03818c8f5e9d7591c34ba7fac064
ShantanuKudva <98833827+ShantanuKudva@users.noreply.github.com>
2024-06-03 18:02:41 +0530

    fixed all bugs - v1.0

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,3 +1,35 @@
+// "use client";
+
+// import * as React from "react";
+// import { useTheme } from "next-themes";
+// import { DarkModeSwitch } from "react-toggle-dark-mode";
+
+// export function ModeToggle() {
+//   const { theme, setTheme, resolvedTheme } = useTheme();
+//   const [isDarkMode, setDarkMode] = React.useState(() => {
+//     React.useEffect(() => {
+//       let resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)");
+//       if (resolvedTheme.matches) {
+//         return true;
+//       }
+//       return false;
+//     }, []);
+//   });
+
+//   React.useEffect(() => {
+//     setDarkMode(resolvedTheme === "dark");
+//   }, [resolvedTheme]);
+
+//   const toggleDarkMode = (checked) => {
+//     setDarkMode(!checked);
+//     setTheme(checked ? "dark" : "light");
+//   };
+
+//   return (
+//     <DarkModeSwitch checked={isDarkMode} onChange={toggleDarkMode} size={30} />
+//   );
+// }
+
 "use client";
 
 import * as React from "react";
@@ -6,13 +38,14 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export function ModeToggle() {
   const { theme, setTheme, resolvedTheme } = useTheme();
-  const [isDarkMode, setDarkMode] = React.useState(() => {
-    let resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)");
-    if (resolvedTheme.matches) {
-      return true;
-    }
-    return false;
-  });
+  const [isDarkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    const resolvedTheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setDarkMode(resolvedTheme);
+  }, []);
 
   React.useEffect(() => {
     setDarkMode(resolvedTheme === "dark");
